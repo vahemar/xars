2d151efbfec18f2d8ec62a25d3f25390c858b76a
vahe.martirosyan.12345 <vahe.martirosyan.12345@gmail.com>
2024-12-27 23:40:19 +0400

    analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <ChakraProvider>
-      <SpeedInsights />
       <Analytics />
       <RouterProvider router={router} />
     </ChakraProvider>
