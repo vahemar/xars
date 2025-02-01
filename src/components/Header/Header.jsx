5ad1a9e117e2ae5a78a88ab3b8217cdfbc4ae31d
vahe.martirosyan.12345 <vahe.martirosyan.12345@gmail.com>
2025-02-01 17:31:17 +0400

    location updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Flex, Image, Text } from '@chakra-ui/react'
 import xarsImage from '../../assets/svg/xarslogo.svg'
-import callIcon from '../../assets/svg/call.svg'
-import locationIcon from '../../assets/svg/location.svg'
 import { useLocation, useNavigate } from 'react-router'
 
 function Header() {
@@ -59,7 +57,9 @@ function Header() {
             <Flex gap={'5px'} direction={'row'} align={'center'}>
               {/* <Image src={locationIcon} width={'30px'} height={'30px'} /> */}
               <Flex color={'#F3E6E6'} direction={'column'}>
-                <a href='https://www.google.com/maps/place/%D5%82%D5%A1%D6%80%D5%BD+%D4%B9%D5%B8%D5%B6%D6%80%D5%A1%D5%BF%D5%B8%D6%82%D5%B6/'><Text _hover={{ color: 'white' }} transition={'0.2s'}>Օհանովի 9/7</Text></a>
+              <a href='https://www.google.com/maps/place/%D5%82%D5%A1%D6%80%D5%BD+%D4%B9%D5%B8%D5%B6%D6%80%D5%A1%D5%BF%D5%B8%D6%82%D5%B6+(Ghars%2FXars+Tonratun)/@40.1804887,44.4458657,18.75z/data=!4m6!3m5!1s0x406abd003d524eeb:0xdcf01adc8feb5539!8m2!3d40.1804333!4d44.4461906!16s%2Fg%2F11lmjpf67j?entry=ttu'>
+              <Text _hover={{ color: 'white' }} transition={'0.2s'} color={'gray.200'}>Օհանովի 9/7</Text>
+            </a>
               </Flex>
             </Flex>
           </Flex>
