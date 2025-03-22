4fc13ea1e8ae28d23f578c7feba24eeda86751bd
vahe.martirosyan.12345 <vahe.martirosyan.12345@gmail.com>
2025-03-23 02:17:16 +0400

    map changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,9 +16,9 @@ function Footer() {
         <Flex align={'center'} direction={'column'}>
           <Flex lineHeight={'17px'} align={'center'} direction={'column'} gap={'4px'}>
             <a href='https://www.google.com/maps/place/%D5%82%D5%A1%D6%80%D5%BD+%D4%B9%D5%B8%D5%B6%D6%80%D5%A1%D5%BF%D5%B8%D6%82%D5%B6+(Ghars%2FXars+Tonratun)/@40.1804887,44.4458657,18.75z/data=!4m6!3m5!1s0x406abd003d524eeb:0xdcf01adc8feb5539!8m2!3d40.1804333!4d44.4461906!16s%2Fg%2F11lmjpf67j?entry=ttu'>
-              <Text _hover={{ color: 'white' }} transition={'0.2s'} color={'#D3D3D3'}>Հայաստան, Երևան, Օհանովի 9/7</Text>
+              <Text _hover={{ color: 'white' }} transition={'0.2s'} color={'#E7E7E7'}>Հայաստան, Երևան, Օհանովի 9/7</Text>
             </a>
-            <Flex color={'#D3D3D3'} direction={'row'} gap={2} align={'center'}>
+            <Flex color={'#E7E7E7'} direction={'row'} gap={2} align={'center'}>
               <Text>Հեռախոսահամար.</Text>
               <a href='tel:+010730771'>
                 <Text _hover={{ color: 'white' }} transition={'0.2s'} as='span'>(010) 730 771 /</Text>
@@ -31,7 +31,8 @@ function Footer() {
               </a>
             </Flex>
           </Flex>
-          <Flex pt={4}>
+          <Flex pt={4} gap={2}>
+          <Image src={tiktokXars} width={'30px'} height={'30px'} cursor={'pointer'} onClick={() => window.open('https://www.instagram.com/xarstonratun', '_blank')}/>
             <Image src={instagramXars} width={'30px'} height={'30px'} cursor={'pointer'} onClick={() => window.open('https://www.instagram.com/xarstonratun', '_blank')}/>
           </Flex>
         </Flex>
