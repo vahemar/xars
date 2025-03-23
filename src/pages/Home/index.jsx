97ad92564dc5101ab28c352710f9eaf020bf15b0
vahe.martirosyan.12345 <vahe.martirosyan.12345@gmail.com>
2025-03-23 17:41:33 +0400

    f

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,20 +9,15 @@ import Location from './Location'
 function Home() {
   return (
     <Flex p={2} gap={8} w={'full'} align={'center'} direction={'column'}>
-      {/* <Attention /> */}
-      <Divider pt={8} w={'full'}  borderColor={'#9A9A9A'} />
+      <Divider pt={8} w={'full'} borderColor={'#9A9A9A'} />
       <Workhours />
-      <Divider w={'full'}  borderColor={'#9A9A9A'} />
+      <Divider w={'full'} borderColor={'#9A9A9A'} />
       <Flex direction={'column'} w={'full'} align={'center'}>
-
-      <Text fontWeight={500} fontSize={'25px'} pb={10}>Տեղանքը և Առաքումը տարածքում</Text>
-    <Location/>
-    </Flex>
-
-      <Divider w={'full'}  borderColor={'#9A9A9A'} />
-      {/* <Delivery /> */}
+        <Text fontWeight={500} fontSize={'25px'} pb={10}>Տեղանքը և Առաքումը տարածքում</Text>
+        <Location />
+      </Flex>
+      <Divider w={'full'} borderColor={'#9A9A9A'} />
       <Building />
-    
     </Flex>
   )
 }
